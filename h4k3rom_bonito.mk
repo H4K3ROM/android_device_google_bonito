320518416ef8bf4da956eaff650b5b0d23870d67
H4K3ROM <44916440+H4K3ROM@users.noreply.github.com>
2021-07-18 11:37:50 -0400

    Update h4k3rom_bonito.mk

--- h4k3rom_bonito.mk
+++ h4k3rom_bonito.mk
@@ -11,14 +11,19 @@ $(call inherit-product, device/google/bonito/aosp_bonito.mk)
 -include device/google/bonito/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := bonito
 PRODUCT_NAME := h4k3rom_bonito
 PRODUCT_MODEL := Pixel 3a XL
 PRODUCT_BRAND := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bonito \
-    PRIVATE_BUILD_DESC="bonito-user 11 RQ3A.210705.001 7380771 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 11   release-keys"
 
-BUILD_FINGERPRINT := google/bonito/bonito:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT := google/bonito/bonito:11/RQ3A.210605.005/7349499:user/release-keys
+
+# H4K3ROM Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="Ike Gilbert (Floyd Flivercod III)"
 
 $(call inherit-product, vendor/google/bonito/bonito-vendor.mk)
